561cf47a5db757c7ee6f67bfd6a4e623daaa320e
tiancsb <tiancsb@hotmail.com>
2013-06-01 12:13:38 -0300

    reboot

--- header.php
+++
@@ -1,8 +0,0 @@
-﻿<div class="row-fluid">
-	<div class="span6">
-		<img src="./img/logo.png" alt="Logo" />
-	</div>
-	<div class="span6 text-right">
-		<img src="img/logoMyS.png" alt="Logo" />
-	</div>
-</div>
--- temp.php
+++
@@ -1,5 +0,0 @@
-<div class="row-fluid">
-	<div class="span12">
-		<div style="height:700px"></div>
-	</div>
-</div>
